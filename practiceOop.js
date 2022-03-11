0f3e3cb66c7079c0f8ceb522292613c9059a1468
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-03-11 17:58:47 +0000

    updated

--- practiceOop.js
+++ practiceOop.js
@@ -64,3 +64,21 @@ user2.logout();
 
 
 //method chaining 
+
+//zellar alogrithom
+let d =  3;
+let m = 7;
+let y = 1991;
+
+let f, l, s, x;
+
+if(m<3){
+  m= m+12;
+  y = y-1;
+}
+f =Math.floor(y/100);
+l = y - 100 * f;
+s =Math.floor(2.6 *m -5.39) + Math.floor(l/4)+ Math.floor(f/4)+ d +1 - (2*f)
+
+x = s -(7 *Math.floor(s/7));
+console.log(x)
